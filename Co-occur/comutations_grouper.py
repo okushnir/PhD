6d1b7adb06e49a4c796fe3a13478dcd6e97d5b1c
okushnir <odedkushnir@gmail.com>
2019-11-07 20:39:07 +0200

    getting out comutations_grouper from the script

--- Co-occur/comutations_grouper.py
+++ Co-occur/comutations_grouper.py
@@ -1,43 +1,44 @@
 import numpy as np
 import pandas as pd
-import matplotlib
-import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 class comutations_bubble(object):
     def __init__(self, a, b, freq, pval):
-        self.nodes=set([a,b])
-        self.distances={'_'.join([str(a),str(b)]):freq, '_'.join([str(b),str(a)]):freq}
-        self.pvalues={'_'.join([str(a),str(b)]):pval, '_'.join([str(b),str(a)]):pval}
-        self.meandist=np.mean(list(self.distances.values()))
+        self.nodes = set([a, b])
+        self.distances = {'_'.join([str(a), str(b)]): freq, '_'.join([str(b), str(a)]): freq}
+        self.pvalues = {'_'.join([str(a), str(b)]): pval, '_'.join([str(b), str(a)]): pval}
+        self.meandist = np.mean(list(self.distances.values()))
 
     def __len__(self):
         return len(self.nodes)
 
     def __repr__(self):
-        return ':'.join([str(a) for a in [len(self),min(self.nodes),max(self.nodes)]])
+        return ':'.join([str(a) for a in [len(self), min(self.nodes), max(self.nodes)]])
 
     def can_merge(self, other, distance=10):
-        return len(self.nodes.intersection(other.nodes)) > 0 and ((other.meandist*distance >= self.meandist) and (other.meandist/distance <= self.meandist))
-    
+        return len(self.nodes.intersection(other.nodes)) > 0 and (
+                    (other.meandist * distance >= self.meandist) and (other.meandist / distance <= self.meandist))
+
     def union(self, other):
-        self.nodes=self.nodes.union(other.nodes)
+        self.nodes = self.nodes.union(other.nodes)
         self.distances.update(other.distances)
         self.pvalues.update(other.pvalues)
-        self.meandist=np.mean(list(self.distances.values())) 
-    
+        self.meandist = np.mean(list(self.distances.values()))
+
+
 def load_file(path, label):
-    df = pd.read_csv(path, sep="\t", names=["start","end","fisher_stat","pval","variant_freq"])
+    df = pd.read_csv(path, sep="\t", names=["start", "end", "fisher_stat", "pval", "variant_freq"])
     df["sample"] = label
-    df = df[df["pval"]<1]
+    df = df[df["pval"] < 1]
     return df
 
-def obtain_comutations(comutations, max_pval=10**-9, distance=10):
-    dfs=[]
+
+def obtain_comutations(comutations, max_pval=10 ** -9, distance=10):
+    dfs = []
     sig_positions = comutations[(comutations["pval"] < max_pval)]
-    
-    lines=sig_positions.itertuples(index=False)
-    nodes=[]
+
+    lines = sig_positions.itertuples(index=False)
+    nodes = []
     for line in lines:
         a = line[0]
         b = line[1]
@@ -45,32 +46,33 @@ def obtain_comutations(comutations, max_pval=10**-9, distance=10):
         dist = line[4]
         node = comutations_bubble(a, b, dist, pval)
         nodes.append(node)
-    
-    results=[]
-    nodes=sorted(nodes, key=lambda item: -item.meandist)
+
+    results = []
+    nodes = sorted(nodes, key=lambda item: -item.meandist)
     while len(nodes) > 0:
-        bubble=nodes.pop(0)
-        merged=False
+        bubble = nodes.pop(0)
+        merged = False
         for item in nodes:
             if item.can_merge(bubble, distance):
                 item.union(bubble)
-                merged=True
+                merged = True
                 break
         if not merged:
             results.append(bubble)
-                
-    if results:
-        #for cluster in sorted(results, key=lambda item: -item.meandist):
-        #    print cluster.meandist, sorted(cluster.nodes)
-        #Collate lines
-        for i, cluster in zip(range(len(results)), sorted(results, key=lambda item: -item.meandist)):
-            #distances=[]
-            distances=map(lambda x: (int(x.split("_")[0]), int(x.split("_")[1]), cluster.distances[x]), cluster.distances.keys())           
-            data = pd.DataFrame(distances, columns=["Pos1", "Pos2", "Freq"])
-            data["Stretch"]=i
 
-            data["Sample"]=comutations.iloc[0]['sample']
-            data["meandist"]=cluster.meandist
+    if results:
+        # for cluster in sorted(results, key=lambda item: -item.meandist):
+        #    print cluster.meandist, sorted(cluster.nodes)
+        # Collate lines
+        for i, cluster in zip(range(len(results)), sorted(results, key=lambda item: -item.meandist)):
+            # distances=[]
+            distances = map(lambda x: (int(x.split("_")[0]), int(x.split("_")[1]), cluster.distances[x]),
+                            cluster.distances.keys())
+            data = pd.DataFrame(distances, columns=["Pos1", "Pos2", "Freq"])
+            data["Stretch"] = i
+
+            data["Sample"] = comutations.iloc[0]['sample']
+            data["meandist"] = cluster.meandist
             dfs.append(data)
-            
+
     return pd.concat(dfs)
