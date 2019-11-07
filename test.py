897cdc3be8fbaf55f6f168e465c0b0d6503f13a7
okushnir <odedkushnir@gmail.com>
2019-11-07 21:09:15 +0200

    20191007

---
+++ test.py
@@ -0,0 +1,35 @@
+import os
+import numpy as np
+import pandas as pd
+
+
+# input_dir = "/Users/odedkushnir/Projects/fitness/CirSeq/PV/OPV/"
+# mutation_lst = ["AG", "AG_adar", "AG_nonadar", "GA", "UC", "CU"]
+# fits_input_dir = input_dir + "fits/input/p1-p7/"
+#
+# jnum = []
+# for mutation in mutation_lst:
+#     list = os.listdir(fits_input_dir + mutation) # dir is your directory path
+#     number_files = len(list)
+#     jnum.append(number_files)
+# print(jnum)
+# jnum = max(jnum)
+# print(jnum)
+# a1 = 1
+# d = 6
+# an = a1+d*(jnum-1)
+#
+# print(an)
+# axes = np.array([[1, 2, 3], [4, 5, 6]], np.int32)
+# print(axes)
+for i, j in range(axes):
+    print(axes[i, j])
+
+
+
+
+
+# print(grouped.first())
+
+
+# print(data.to_string())
