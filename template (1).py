fb3f75f23e853c6298c031310735ee0040657a9e
okushnir <odedkushnir@gmail.com>
2019-11-07 21:01:22 +0200

    Modify to argparse

--- template (1).py
+++ template (1).py
@@ -5,7 +5,7 @@
 
 """
 
-import sys
+import sys, argparse
 import matplotlib
 matplotlib.use('Agg') # Must be before importing matplotlib.pyplot or pylab!
 import pandas as pd
@@ -18,19 +18,19 @@ import Bio.Seq as Seq
 import matplotlib.gridspec as gridspec
 import time
 
-def main():
-    parser = OptionParser("usage: %prog [options]")
-    parser.add_option("-f", "--file1", dest="file1", help="fastq1 file")
-    parser.add_option("-e", "--file2", dest="file2", help="fastq2 file")
-    parser.add_option("-o", "--output_file", dest="output_file", help="output fastq merged file")
-    (options, args) = parser.parse_args()
-    file1 = options.file1
-    file2 = options.file2
-    output_file = options.output_file
-
-
 def freqs_position_plot(file_name):
+    # kjkdd
 
+def main(args):
+    # khjdkj
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("virus", type=str, help="name of the virus, RVB14")
+    parser.add_argument("passages", type=str, help="from which passages, p0-p12")
+    parser.add_argument("without", type=int, help="Exclude passage no.")
+    parser.add_argument("input_dir", type=str, help="the path to the directory that contains data_mutation.csv")
+    parser.add_argument("quality", type=str, help="what is the prefix for the data_mutation.csv file; quality of the pipline ; for example: q38")
+    parser.add_argument("mutation_type", type=str, help="all/syn")
+    args = parser.parse_args(sys.argv[1:])
+    main(args)
