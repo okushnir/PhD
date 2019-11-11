5b58f5db53f694e3fbe1716b778e871196b3c544
Oded Kushnir <odedkushnir@gmail.com>
2019-11-11 12:37:09 +0200

    Update 20191111 groupby

--- Co-occur/co_occur_plots.py
+++ Co-occur/co_occur_plots.py
@@ -14,18 +14,32 @@ import seaborn as sns
 
 def main():
     input_dir = "/Volumes/STERNADILABHOME$/volume3/okushnir/AccuNGS/190627_RV_CV/merged/RVB14"
+    output_dir = "/Users/odedkushnir/Projects/fitness/AccuNGS/190627_RV_CV/RVB14/20191111_plots"
     data_p2 = pd.read_csv(input_dir + "/RVB14_p2/20191029_q38/co_occur.csv")
     data_p5 = pd.read_csv(input_dir + "/RVB14_p5/20191029_q38/co_occur.csv")
     data_p8 = pd.read_csv(input_dir + "/RVB14_p8/20191029_q38/co_occur.csv")
     data_p10 = pd.read_csv(input_dir + "/RVB14_p10/20191029_q38/co_occur.csv")
-    # data_p12 = pd.read_csv(input_dir + "/RVB14_p12/20191029_q38/co_occur.csv")
-    df = pd.concat([data_p2, data_p5, data_p8, data_p10])
+    data_p12 = pd.read_csv(input_dir + "/RVB14_p12/20191029_q38/co_occur.csv")
+    df = pd.concat([data_p2, data_p5, data_p8, data_p10, data_p12])
+
+    # df_co_occur = df.loc[(df.Pos == df.Pos)  (df.label != df.label)]
+    # df_co_occur = df_co_occur.sort_values(by=["Pos"])
+    # grouped = df.groupby(["Pos"], as_index=False).count()
+    # df_co_occur = df.groupby(["Pos", "label"]).reest_index(name="Count")
+    grouped = df.groupby(["Pos"])
+    df_co_occur = pd.DataFrame(grouped.size().reset_index(name="Group_Count"))
+    df_co_occur = df_co_occur.merge(df, how="outer", on="Pos")
+
+    df_co_occur = df_co_occur.loc[df_co_occur.Group_Count > 1]
+    df_co_occur.to_csv(input_dir + "/all_co_occur.csv", sep=",", encoding='utf-8')
 
     #Plot
-    g1 = sns.relplot(x="Pos", y="Frequency", data=df, hue="label")#, style="Stretch")
+    g1 = sns.relplot(x="Pos", y="Frequency", data=df_co_occur, hue="label", col="Group_Count", col_wrap=2)#, style="Stretch")
     g1.set(yscale="log")
-    plt.show()
-    mnkdd
+    # plt.show()
+    g1.savefig(output_dir + "/co_occur.png", dpi=300)
+
+
 
 if __name__ == "__main__":
     # parser = argparse.ArgumentParser()
