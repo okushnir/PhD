84b2aeb7ed6570b32a3f67c664ccdeede93808dc
okushnir <odedkushnir@gmail.com>
2020-11-11 19:00:52 +0200

    20201111 Update

--- AccuNGS_analysis/adar_mutation_palette.py
+++ AccuNGS_analysis/adar_mutation_palette.py
@@ -1,4 +1,4 @@
-
+import seaborn as sns
 # flatui = ["#3498db", "#9b59b6"]
 
 def checkKey(dict, key):
@@ -8,7 +8,7 @@ def checkKey(dict, key):
         raise Exception()
 
 
-def mutation_palette(color_num, adar=False, ag=None, uc=None):
+def mutation_palette(color_num, adar=False, ag=None, uc=None, gray=None):
     """
     :param color_num: How many colors in the plot
     :param adar: ADAR - True/False
@@ -16,9 +16,10 @@ def mutation_palette(color_num, adar=False, ag=None, uc=None):
     :param cu: cu - True/False
     :return: palette
     """
-    palette_dict = {"High\nADAR-like\nA>G": "#3E0E1F", "Intermediate\nADAR-like\nA>G": "#D11E3C", "Low\nADAR-like\nA>G":
-        "#FA9009", "High\nADAR-like\nU>C": "#37B3FF", "Intermediate\nADAR-like\nU>C": "#83D0FF", "Low\nADAR-like\nU>C":
-                        "#D4EDFC", "G>A": "#70EC57", "C>U": "#FEF601"}
+    palette_dict = {"High\nADAR-like\nA>G": sns.color_palette("muted")[0], "Intermediate\nADAR-like\nA>G": "#52BBFA",
+                    "Low\nADAR-like\nA>G": "#C8E9FE", "High\nADAR-like\nU>C": "#FE0F00",
+                    "Intermediate\nADAR-like\nU>C": "#D11E3C", "Low\nADAR-like\nU>C": "#D4EDFC", "G>A": "#38D04F",
+                    "C>U": "#FA9009", "gray": "#D7DADC"}
     palette = None
     if color_num == 2:
         true_false_palette = [checkKey(palette_dict, "Intermediate\nADAR-like\nA>G"), checkKey(palette_dict, "C>U")]
@@ -28,7 +29,7 @@ def mutation_palette(color_num, adar=False, ag=None, uc=None):
                              checkKey(palette_dict, "Intermediate\nADAR-like\nU>C"), checkKey(palette_dict, "G>A"),
                              checkKey(palette_dict, "C>U")]
         palette = four_mutation_palette
-    elif color_num == 8:
+    elif ((color_num == 8) & (gray==None)):
         eight_mutation_palette = [checkKey(palette_dict, "High\nADAR-like\nA>G"),
                                   checkKey(palette_dict, "Intermediate\nADAR-like\nA>G"),
                                   checkKey(palette_dict, "Low\nADAR-like\nA>G"),
@@ -38,6 +39,17 @@ def mutation_palette(color_num, adar=False, ag=None, uc=None):
                                   checkKey(palette_dict, "G>A"),
                                   checkKey(palette_dict, "C>U")]
         palette = eight_mutation_palette
+
+    elif ((color_num == 8) & (gray==True)):
+        eight_mutation_palette_gray = [checkKey(palette_dict, "High\nADAR-like\nA>G"),
+                                  checkKey(palette_dict, "gray"),
+                                  checkKey(palette_dict, "gray"),
+                                  checkKey(palette_dict, "gray"),
+                                  checkKey(palette_dict, "gray"),
+                                  checkKey(palette_dict, "gray"),
+                                  checkKey(palette_dict, "gray"),
+                                  checkKey(palette_dict, "gray")]
+        palette = eight_mutation_palette_gray
     elif ((color_num == 3) & (adar == True) & (ag == True)):
         adar_ag_palette = [checkKey(palette_dict, "High\nADAR-like\nA>G"),
                            checkKey(palette_dict, "Intermediate\nADAR-like\nA>G"),
