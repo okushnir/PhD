2020e6ae58b7092ce16f2ba811eac5e65eb531e8
odedkushnir@gamil.com <Arubh214>
2022-01-06 13:23:25 +0200

    20220106 Update

--- Co-occur/fisher_test_reads.py
+++ Co-occur/fisher_test_reads.py
@@ -8,32 +8,47 @@
 import sys, argparse
 import pandas as pd
 import numpy as np
-from string import digits
+from textwrap import wrap
+import scipy.stats as stats
 
-def pos_next_line(data, i):
-    pos_next_line = data["Pos"].iloc[i+1]
-    return pos_next_line
 
-def main():
+def string_delimiter(string):
+    string_lst = wrap(string, 2)
+    return string_lst
 
-    input_dir = "/Users/odedkushnir/Google Drive/Studies/PhD/test"
-    data = pd.read_table(input_dir + "/p2_1/20201012_q38/all_parts.blast", sep="\t")
 
+def AG_read_counter(data):
     data = pd.DataFrame(data.columns.values[None, :], columns=data.columns).append(data).reset_index(drop=True)
     data.columns = range(data.shape[1])
     columns = ["read", "start_pos", "end_pos", "start_read", "end_read", "direction", "read_len", "blast_out"]
     df = pd.DataFrame(data.values, columns=columns)
     df1 = df[["read", "blast_out"]]
     df1["new_blast_out"] = df1["blast_out"].str.replace('\d+', '')
-    # TODO:
-    # count the AG in new_blast_out
-    # if >3 True
-    # count True/False"""
+    df1["new_blast_out"] = df1["new_blast_out"].apply(lambda x: string_delimiter(x))
+    df1["new_blast_out_count"] = df1["new_blast_out"].apply(lambda x: x.count("AG"))
+    df1["in_stretch"] = np.where(df1["new_blast_out_count"] >= 3, True, False)
+    df_grouped = df1.groupby("in_stretch", as_index=False).count()
+    df_grouped = df_grouped[["in_stretch", "read"]]
+    df_grouped = df_grouped.rename(columns={"read": "count"})
+    df_grouped = df_grouped.set_index("in_stretch")
+    return df1, df_grouped
 
-    # df_grouped = df1.groupby("new_blast_out", as_index=False).count()
-
-
-    print(df1.to_string())
+def main():
+    # input_dir = "/Users/odedkushnir/Google Drive/Studies/PhD/test"
+    input_dir = "C:/Users/odedku/test"
+    data_control = pd.read_table(input_dir + "/IVT_3_Control/20201012_q38/all_parts.blast", sep="\t")
+    data_p2_1 = pd.read_table(input_dir + "/p2_1/20201012_q38/all_parts.blast", sep="\t")
+    df_control, df_control_grouped = AG_read_counter(data_control)
+    df_p2_1, df_p2_1_grouped = AG_read_counter(data_p2_1)
+    # print(df_control.to_string())
+    # print(df_p2_1.to_string())
+    # crossta_df = pd.crosstab(index=df_control["in_stretch"], columns=df_p2_1["in_stretch"])
+    crosstab_df = pd.merge(df_control_grouped, df_p2_1_grouped, on=df_p2_1_grouped.index)
+    crosstab_df = crosstab_df.set_index("key_0")
+    crosstab_df = crosstab_df.rename(columns={"count_x": "Control", "count_y": "Test"})
+    oddsratio, pval = stats.fisher_exact(crosstab_df, alternative="two-sided")
+    print(crosstab_df.to_string())
+    print("oddsratio={0}    pval={1}".format(oddsratio, pval))
 
 if __name__ == "__main__":
     main()
