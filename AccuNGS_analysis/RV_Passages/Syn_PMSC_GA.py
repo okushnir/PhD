aa9f392c4ebd3b7354797e492bb64fa0b8c71d20
odedku <odedkushnir@gmail.com>
2022-05-26 00:59:15 +0300

    20220526 Update

--- AccuNGS_analysis/RV_Passages/Syn_PMSC_GA.py
+++ AccuNGS_analysis/RV_Passages/Syn_PMSC_GA.py
@@ -9,16 +9,18 @@ def main():
     freq = pd.read_pickle("D:/My Drive/Studies/PhD/Projects/RV/RVB14/p12_1/data_filter.pkl")
     replica = 3
     table = freq.loc[freq["replica"] != replica]
+    first_quantile = table["Frequency"].quantile(0.25)
+    last_quantile = table["Frequency"].quantile(0.75)
+    table = table[((table["Frequency"] >= first_quantile) | (table["Frequency"] <= last_quantile))]
     # table["Filter"] = np.where(table["passage"] == 2, True, np.where(table["passage"] == 5, True, False))
     # table = table.loc[table["Filter"] == True]
     transition_order = ["A>G", "U>C", "G>A", "C>U"]
     table["Mutation_Type"] = np.where(table["Mutation"] == "A>G", "transition", np.where(table["Mutation"] == "U>C", "transition",
                                                                                          np.where(table["Mutation"] == "G>A", "transition",
                                                                                                   np.where(table["Mutation"] == "C>U", "transition", "transversion"))))
+
+
     table = table.loc[table["Mutation_Type"] == "transition"]
-    first_quantile = table["Frequency"].quantile(0.25)
-    last_quantile = table["Frequency"].quantile(0.75)
-    table = table[((table["Frequency"] >= first_quantile) | (table["Frequency"] <= last_quantile))]
     fig1 = sns.catplot(y="Frequency", x="Mutation", data=table, hue="Type", kind="box", col="Mutation_Type",
                        hue_order=["Synonymous", "Premature Stop Codon", "Non-Synonymous"], order=transition_order)
     fig1.set(yscale='log', ylim=(10 ** -5, 10 ** -2))
