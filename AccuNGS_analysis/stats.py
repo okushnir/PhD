818e57bb74ac223c9666849befd9bb0c660341f3
okushnir <odedkushnir@gmail.com>
2022-10-18 22:08:17 +0300

    20221018 Update

--- AccuNGS_analysis/stats.py
+++ AccuNGS_analysis/stats.py
@@ -47,7 +47,7 @@ def iter_stat(inputdir_dir, sheet, rv_sheet=None):
 def sts_fits(file_path, new_file, skiprows=6):
     with open(file_path, "r") as stsfile, open(new_file, 'w') as outfile:
         for line in stsfile:
-            new_line = line.replace(": Mann-Whitney-Wilcoxon test greater with Bonferroni correction", "").replace("P_val:", "").replace("U_stat=", ", ")
+            new_line = line.replace(": Welch's t-test independent samples with Bonferroni correction", "").replace("P_val:", "").replace("t=", ", ")
             outfile.write(new_line)
     outfile.close()
     sts_df = pd.read_csv(new_file, skiprows=skiprows, header=None)
@@ -56,7 +56,7 @@ def sts_fits(file_path, new_file, skiprows=6):
     #                                  np.where((sts_df["p-value"] >= 0.001) & (sts_df["p-value"] <= 0.01), "**",
     #                                           np.where((sts_df["p-value"] >= 0.0001) & (sts_df["p-value"] <= 0.001), "***",
     #                                                    np.where(sts_df["p-value"] <= 0.0001, "****", "ns"))))
-    sts_df["Test"] = "Mann-Whitney-Wilcoxon test greater with Bonferroni correction"
+    sts_df["Test"] = "Welch's t-test independent samples with Bonferroni correction"
     sts_df.drop("stat", axis=1, inplace=True)
     sts_df = sts_df[["Types of mutations compared", "p-value", "Test"]]
     sts_df = sts_df.sort_values(by="Types of mutations compared",  key=lambda x: np.argsort(index_natsorted(sts_df["Types of mutations compared"])))
@@ -81,11 +81,11 @@ def main():
     # sts_all_df = iter_stat(capsid_dir, sheet="sts_adar_synon", rv_sheet="sts_adar_nonsynon")
     # sts_all_df.to_csv("C:/Users/odedku/PhD_Projects/After_review/Stats/sts_capsid_adar_all.csv")
 
-    # input_dir = "D:/My Drive/Studies/PhD/Thesis/FinalVer/AfterReview/20220921_fits_syn_plots"
-    # sts_file = input_dir + "/sts.csv"
-    # new_file = input_dir + "/sts_edited.csv"
-    # sts_fits_df = sts_fits(sts_file, new_file)
-    # sts_fits_df.to_csv("D:/My Drive/Studies/PhD/Thesis/FinalVer/AfterReview/20220921_fits_syn_plots/sts_fits_df.csv")
+    input_dir = "D:/My Drive/Studies/PhD/Thesis/FinalVer/AfterReview/20221018_fits_syn_plots"
+    sts_file = input_dir + "/sts.csv"
+    new_file = input_dir + "/sts_edited.csv"
+    sts_fits_df = sts_fits(sts_file, new_file)
+    sts_fits_df.to_csv("D:/My Drive/Studies/PhD/Thesis/FinalVer/AfterReview/20221018_fits_syn_plots/sts_fits_df.csv")
 
 
 if __name__ == "__main__":
